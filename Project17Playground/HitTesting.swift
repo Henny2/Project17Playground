5343efe91961a61a50beb9b9e3a8edb4206fed36
Henny2 <henrieke.baunack@web.de>
2024-03-28 17:52:01 -1000

    adding contentShape and allowsHittesting Examples

--- Project17Playground/HitTesting.swift
+++ Project17Playground/HitTesting.swift
@@ -18,9 +18,17 @@ struct HitTesting: View {
             Circle()
                 .fill(.red)
                 .frame(width:200, height: 200)
+            // other option is contentShape modifier that lets us modify the shape that reacts to the taps
+            // contentShape helps when you have Spacers() that usually would not be tappable
+            //https://www.hackingwithswift.com/books/ios-swiftui/disabling-user-interactivity-with-allowshittesting
+                .contentShape(.rect)
                 .onTapGesture {
                     print("Circle tapped")
                 }
+                //.allowsHitTesting(/*@START_MENU_TOKEN@*/false/*@END_MENU_TOKEN@*/) // this will make the circle not react to any taps, we will tapp what is behind
+            
+            
+            
         }
     }
 }
